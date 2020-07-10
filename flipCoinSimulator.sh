13540a895f9b26397b63244ed4df4238ead54e22
unknown <snehanimbare15@gmail.com>
2020-07-10 20:43:54 +0530

    Multiple times of flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash
 
-num=$((RANDOM%2))
-if [[ $num -eq 0 ]]
-     then
-	echo Heads
-     else
-        echo Tails
-fi
+read -p "Enter Limit to win :" Limit
+
+while [ true ]
+do
+        num=$((RANDOM%2))
+        if [[ $num -eq 0 ]]
+        then
+		echo Heads
+                ((Headcount++))
+        else
+                echo Tails
+                ((Tailcount++))
+        fi
+        if [[ $Headcount -eq $Limit ]]
+        then
+                echo -e "\nHeads have been reached $Limit times"
+		echo "And won by $(($Limit-$Tailcount)) points"
+	        break
+        fi
+        if [[ $Tailcount -eq $Limit ]]
+        then
+                echo -e "\nTails have been reached $Limit times"
+                echo "And won by $(($Limit-$Headcount)) points"
+                break
+        fi
+
+done
